a243b6b001fe775a17b17eec395663b2ec06ddc0
Andresssg <andres-guevara1@upc.edu.co>
2023-05-26 13:21:51 -0500

    Change class to classname

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -6,7 +6,7 @@ function CategoryCard ({ icon, category, color }) {
     lg:p-10 border-4 border-gray-200 rounded-xl'
       onClick={() => { window.alert(`La categoria ${category} fue seleccionada`) }}
     >
-      <div class={`flex w-36 h-36 bg-${color}-500 rounded-full items-center justify-center`}>
+      <div className={`flex w-36 h-36 bg-${color}-500 rounded-full items-center justify-center`}>
         <img className='h-full w-full object-contain' src={icon} alt='icono categoria' />
       </div>
       <h2 className='text-2xl font-bold'>{category}</h2>
