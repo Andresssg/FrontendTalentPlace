78d04c5fda979e4653ac7f7cda4a223300ca0489
Andresssg <andres-guevara1@upc.edu.co>
2023-05-28 15:41:48 -0500

    Fix responsive in resgister card

--- src/components/ResgisterCard.jsx
+++ src/components/ResgisterCard.jsx
@@ -3,9 +3,9 @@ import image from '../assets/resgister_image.jpg'
 
 function RegisterCard () {
   return (
-    <section className='flex justify-left items-center h-200px md:h-[500px] lg:h-[50vh] lg:w-[50vw] bg-purple-500 relative md:rounded-xl overflow-hidden'>
+    <section className='flex justify-left items-center h-200px md:h-[500px] lg:h-[50vh] lg:w-[50vw] bg-purple-500 relative lg:rounded-xl overflow-hidden'>
       <img className='w-full h-full absolute' src={image} alt='Image banner' />
-      <div className='flex flex-col justify-center gap-10 px-20 relative font-light h-full w-full text-3xl md:text-5xl bg-opacity-60 text-white bg-black'>
+      <div className='flex flex-col justify-evenly px-20 relative font-light h-full w-full text-3xl md:text-5xl bg-opacity-60 text-white bg-black'>
         <h2>¿Alma de emprendor o necesitas ayuda?</h2>
         <p>No importa, este es tu lugar</p>
         <div className='flex justify-center border-4 border-yellow-500 w-fit font-medium p-3 rounded-xl hover:bg-yellow-500 cursor-pointer'>
