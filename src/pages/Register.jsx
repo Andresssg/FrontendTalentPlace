dcf9bfd45f28029e60d7c22c0c23957d9d07d720
Andresssg <andres-guevara1@upc.edu.co>
2023-06-04 02:20:53 -0500

    Add blurred forms

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -40,7 +40,13 @@ function Register () {
   }
   return (
     <div className='min-h-screen w-full bg-[#f7f9fb] flex flex-col lg:flex-row justify-evenly items-center p-10'>
-      <section className=''>
+      <section className='relative'>
+        {/* eslint-disable-next-line react/self-closing-comp */}
+        <div className='absolute top-0 -left-4 w-72 h-72 bg-red-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob'></div>
+        {/* eslint-disable-next-line react/self-closing-comp */}
+        <div className='absolute top-0 md:left-60 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animation-delay-2000'></div>
+        {/* eslint-disable-next-line react/self-closing-comp */}
+        {/* <div class='absolute -bottom-8 left-20 w-72 h-72 bg-red-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animation-delay-4000'></div> */}
         <h1 className='text-5xl lg:text-6xl'>Registrate para comenzar<br />esta experiencia</h1>
         <p className='text-xl lg:text-3xl'>Si tienes una cuenta <span className='text-red-500'>Inicia sesión</span></p>
       </section>
